70700b37d545d94d4c321d64f05c5c48663aae85
maybe-vibe <268501654+maybe-vibe@users.noreply.github.com>
2026-09-10 04:31:58 +0000

    Update dashboard-artifact to output generated at cc9c4c8

---
+++ _next/static/JfAZ2ngjZX5QUgOK6iUFo/_buildManifest.js
@@ -0,0 +1,90 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/30ylhhnnws9y9.js"
+  ],
+  "/_error": [
+    "static/chunks/2l6m9v-0p50zy.js"
+  ],
+  "/await_tree": [
+    "static/chunks/2cmd07ie9j5e8.js"
+  ],
+  "/batch_tasks": [
+    "static/chunks/3ax7cdjxxh5gv.js"
+  ],
+  "/cluster": [
+    "static/chunks/00ly74u7i0aby.js"
+  ],
+  "/cpu_profiling": [
+    "static/chunks/0k89ocx_446ez.js"
+  ],
+  "/explain_distsql": [
+    "static/chunks/2kpahshekom64.js"
+  ],
+  "/fragment_graph": [
+    "static/chunks/3w-melw_5sw_n.js"
+  ],
+  "/functions": [
+    "static/chunks/0id82frrka3hh.js"
+  ],
+  "/heap_profiling": [
+    "static/chunks/0dsnl_-rc9lk4.js"
+  ],
+  "/indexes": [
+    "static/chunks/3z7wplpjfzb20.js"
+  ],
+  "/internal_tables": [
+    "static/chunks/1bxhh37bwtef2.js"
+  ],
+  "/materialized_views": [
+    "static/chunks/0bfkbbx6d8699.js"
+  ],
+  "/relation_graph": [
+    "static/chunks/296i0w7_dw1jx.js"
+  ],
+  "/settings": [
+    "static/chunks/3n-y8719i3-zt.js"
+  ],
+  "/sinks": [
+    "static/chunks/3vbcplq7_b7sm.js"
+  ],
+  "/sources": [
+    "static/chunks/3m4hnmp24pa_m.js"
+  ],
+  "/subscriptions": [
+    "static/chunks/2xmcfgz3ya6ja.js"
+  ],
+  "/tables": [
+    "static/chunks/2bojyl639b_yp.js"
+  ],
+  "/views": [
+    "static/chunks/1-pc7ipupm66o.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/await_tree",
+    "/batch_tasks",
+    "/cluster",
+    "/cpu_profiling",
+    "/explain_distsql",
+    "/fragment_graph",
+    "/functions",
+    "/heap_profiling",
+    "/indexes",
+    "/internal_tables",
+    "/materialized_views",
+    "/relation_graph",
+    "/settings",
+    "/sinks",
+    "/sources",
+    "/subscriptions",
+    "/tables",
+    "/views"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
